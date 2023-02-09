4f9d82a655cb406943bc1272a2052fb4b59beff6
jmrfG <jorge_ramos13@outlook.com.br>
2023-02-09 07:32:20 -0300

    v.1 final

--- src/components/Navbar/Nav.tsx
+++ src/components/Navbar/Nav.tsx
@@ -4,14 +4,7 @@ const Nav = () => {
   return (
     <nav className="nav p-3 border-bottom">
       <Link href="/" passHref>
-        <h2 className="pointer">Jorge MR Farias</h2>
-      </Link>
-
-      <Link href="/math" passHref>
-        <p className="ms-5 pointer my-auto">Mathematics</p>
-      </Link>
-      <Link href="/philosophy" passHref>
-        <p className="ms-5 pointer my-auto">Philosophy</p>
+        <h2 className="pointer">Jorge Marcos Ramos de Farias</h2>
       </Link>
     </nav>
   )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,54 +1,46 @@
-import Image from 'next/image'
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
+import axios from "axios";
 import Link from 'next/link'
 
 
-export default function Home({ posts }) {
+export default function Home(props) {
+  let data = props.posts.data
   return (
-    <div className="mt-5">
-      <p className="display-4 text-center">Welcome</p>
-      <p className="text-center mt-5">
-        The pursuit of knowledge is a journey divine,<br></br>
-        An adventure that is both challenging and fine.<br></br>
-        With every step taken, the mind doth grow,<br></br>
-        As wisdom and insight begin to flow.<br></br>
-        <br></br><br></br>
-        The journey is never-ending, full of grace,<br></br>
-        An ever-unfolding treasure trove of space.<br></br>
-        For knowledge is the key to unlock doors,<br></br>
-        To new and wondrous worlds never before.<br></br>
-        <br></br><br></br>
-        And in this quest, one finds true beauty, too,<br></br>
-        A splendor that shines like the morning dew.<br></br>
-        For knowledge is power, a precious gem,<br></br>
-        That enriches the soul and lifts the heart to win.<br></br>
-        <br></br><br></br>
-        So heed the call to pursue knowledge with might,<br></br>
-        For it is a journey that will bring endless delight.<br></br>
-        It is a path to follow, a destiny bright,<br></br>
-        That leads to a place of peace, love and light.<br></br>
-      </p>
+    <div className="mt-4">
+        <p className="display-3 text-center">Some Thoughts</p>
+        <div style={{ color: 'black', maxWidth:'540px', margin: "0 auto"}}>
+            {data.map((post) => (
+                <Link href={'/content/' + post.id} passHref key={post.id}>
+                    <div className="card mb-4 pointer" style={{ maxWidth: '540px' }}>
+                        <div className="row g-0">
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title">{post.attributes.title}</h5>
+                                    <p className="card-text">{post.attributes.description}</p>
+                                    <p className="card-text">
+                                        <small className="text-muted">Published At: {post.attributes.publishedAt.split("T")[0]}</small>
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="col-md-4 m-auto">
+
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+            ))}
+        </div>
+
     </div>
-  )
+)
 }
 
 
 export const getStaticProps = async () => {
   //getting data locally, but, if data is stored else where, i could do an API call here
-  const files = fs.readdirSync(path.join('src', 'Posts', 'General'))
-  const posts = files.map(file => {
-    const mdWMeta = fs.readFileSync(path.join('src', 'Posts', 'General', file));
-    const { data: frontmatter } = matter(mdWMeta);
-    return {
-      frontmatter,
-      slug: file.split('.')[0]
-    }
-  })
+  const posts = await axios.get("https://strapi-production-1c44.up.railway.app/api/posts")
   return {
-    props: {
-      posts
-    }
+      props: {
+          posts: posts.data
+      }
   };
-} 
+}
